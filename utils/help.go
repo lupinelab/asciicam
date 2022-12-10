bf15bfc935cefd1d60fabafac5a44171db65cc19
Jed Williamson <jed.williamson@public-i.info>
2022-12-10 14:32:50 +0000

    add help message

--- utils/help.go
+++ utils/help.go
@@ -1,12 +1,13 @@
 package utils
 
 var Help = []string{
-"Brightness [Down]/[Up] ",
-"Contrast   [Left]/[Right] ",
-"Red        [e]/[r] ",
-"Green      [f]/[g] ",
-"Blue       [v]/[b] ",
-"Stats      [i] ",
-"Help       [h] ",
-"Quit       [Esc] ",
+"Brightness  [Down]/[Up] ",
+"Contrast    [Left]/[Right] ",
+"Red         [e]/[r] ",
+"Green       [f]/[g] ",
+"Blue        [v]/[b] ",
+"Colour Mode [m]",	
+"Info        [i] ",
+"Help        [h] ",
+"Quit        [Esc] ",
 }
